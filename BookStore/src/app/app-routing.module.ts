6e6ade86009d26feec435292d6e05e1b90e2cee4
Uddesh <uddeshpatil2001@gmail.com>
2024-02-27 00:10:57 +0530

    [Uddesh]Add: Display book component.

--- BookStore/src/app/app-routing.module.ts
+++ BookStore/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashBoardComponent } from './Components/dash-board/dash-board.component';
+import { DisplayBookComponent } from './Components/display-book/display-book.component';
 
 const routes: Routes = [
   {path:"", component:DashBoardComponent},
+  {path:"display-book", component:DisplayBookComponent}
 ];
 
 @NgModule({
--- BookStore/src/app/app.module.ts
+++ BookStore/src/app/app.module.ts
@@ -10,12 +10,14 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { DisplayBookComponent } from './Components/display-book/display-book.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BookstoreHeaderComponent,
-    DashBoardComponent
+    DashBoardComponent,
+    DisplayBookComponent
   ],
   imports: [
     BrowserModule,
